4a3663b8551dd1846af16408062f7604db64b09e
Profero Ltd. <cpan@profero.com>
2003-10-15 16:29:43 +0000

    Import of PROFERO/Test-CGI-Untaint-1.10 from CPAN.
    gitpan-cpan-distribution: Test-CGI-Untaint
    gitpan-cpan-version:      1.10
    gitpan-cpan-path:         PROFERO/Test-CGI-Untaint-1.10.tar.gz
    gitpan-cpan-author:       PROFERO
    gitpan-cpan-maturity:     released

--- lib/Test/CGI/Untaint.pm
+++ lib/Test/CGI/Untaint.pm
@@ -15,14 +15,15 @@ use CGI::Untaint;
 
 # export the test functions
 use Exporter;
-use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $config_vars $VERSION);
+use vars qw(@ISA @EXPORT @EXPORT_OK %EXPORT_TAGS $config_vars $VERSION @Data_Stack);
 @ISA         = qw( Exporter );
-@EXPORT      = qw( is_extractable unextractable );
+@EXPORT      = qw( is_extractable unextractable
+                   is_extractable_deeply is_extractable_isa );
 @EXPORT_OK   = qw( config_vars );
 %EXPORT_TAGS = ("all" => [ @EXPORT, @EXPORT_OK ]);
 
 # set the version
-$VERSION = 1.01;
+$VERSION = "1.10";
 
 =head1 NAME
 
@@ -45,7 +46,7 @@ The B<CGI::Untaint> module can be extended with "Local Extraction
 Handlers" that can be used define new ways of untainting data.
 
 This module is designed to test these data extraction modules.  It
-does this with the following two methods:
+does this with the following methods:
 
 =over 4
 
@@ -139,6 +140,175 @@ sub unextractable
   return !$result;
 }
 
+=item is_extractable_deeply
+
+Tests that first value passed has the second value passed extracted
+from it when the local extraction handler named in the third argument
+is called B<deeply>.  Where C<is_extractable> does a simple string
+equality test, this does a proper deep check like C<is_deeply> in
+B<Test::More>.  This is most useful when your class returns a big
+old data structure from is_valid rather than a simple scalar.
+
+=cut
+
+sub is_extractable_deeply
+{
+  # extract the params, have a default test name
+  my ($data, $wanted, $func, $name) = @_;
+
+  # default name
+  $name ||= "'$data' deeply extractable as $func";
+
+  # create a CGI::Untaint object
+  my $untaint = CGI::Untaint->new(config_vars(),
+				  data => $data);
+
+  my $result = $untaint->extract("-as_$func" => "data");
+
+  # check if there was an error
+  if ($untaint->error)
+  {
+     $Test->ok(0,$name);
+     $Test->diag($untaint->error);
+     return 0;
+  }
+
+  # The code for the rest of this function is borrowed from
+  # Test::More.
+
+  # variable to store the success or failure
+  my $ok;
+
+  # hang on, are these things both not refs?
+  if( !ref $result || !ref $wanted ) {
+    $ok = $Test->is_eq($result, $wanted, $name);
+  }
+
+  else
+  {
+    # do the deep check
+    local @Data_Stack = ();
+    if (_deep_check($result, $wanted))
+    {
+      # yey! it worked
+      $ok = $Test->ok(1, $name);
+    }
+    else
+    {
+      # no it didn't, darn!
+      $ok = $Test->ok(0, $name);
+      $ok = $Test->diag(_format_stack(@Data_Stack));
+    }
+  }
+
+  # return the value
+  return $ok;
+}
+
+=item is_extractable_isa
+
+Tests that the first value pass extracts something that is, or is
+a subclass of, the class passed in the second argument when the
+extraction handler .
+
+=cut
+
+sub is_extractable_isa
+{
+  my ($data, $class, $func, $name) = @_;
+
+  # default name
+  $name ||= "'$data' extractable as a '$class'";
+
+  # create a CGI::Untaint object
+  my $untaint = CGI::Untaint->new(config_vars(),
+				  data => $data);
+
+  my $object = $untaint->extract("-as_$func" => "data");
+
+  # check if there was an error
+  if ($untaint->error)
+  {
+     $Test->ok(0,$name);
+     $Test->diag($untaint->error);
+     return 0;
+  }
+
+  # the code for the rest of this function is stolen pretty much
+  # wholeheartedly from Test::More.  It's been reformatted to my
+  # style and I've added lots of comments.
+
+  my $diag;
+
+  # check if the object is defined
+  if (!defined $object)
+    { $diag = "the extracted object isn't defined"; }
+
+  # check if the object is a ref
+  elsif (!ref $object)
+    { $diag = "the extracted object isn't a reference"; }
+
+  # check if we can call isa on it
+  else
+  {
+    # We can't use UNIVERSAL::isa because we want to honor isa() overrides
+    local($@, $!);  # eval sometimes resets $!
+
+    # try calling isa
+    my $rslt = eval { $object->isa($class) };
+
+    # did we get an error?
+    if ($@)
+    {
+      # see if it's a error due to the thing being a ref rather than the
+      # thing being an object
+      if ($@ =~ /^Can't call method "isa" on unblessed reference/)
+      {
+	# hmm looks like it''s just a plain old ref.  Use UNIVERSAL::isa
+	# to check we get the same thing
+	if (!UNIVERSAL::isa($object, $class))
+        {
+	  my $ref = ref $object;
+          $diag = "the extracted object isn't a '$class' it's a '$ref'";
+        }
+      }
+      else
+      {
+	# We got a error thrown from the code when we called the isa
+	# method?  That's screwed up!  PANIC!
+	die <<WHOA;
+WHOA! I tried to call ->isa on the extacted object and got some weird
+error. This should never happen.  Please contact the author immediately.
+Here's the error.
+$@
+WHOA
+       }
+    }
+
+    # did we get false back?  That means it's a real object, but it
+    # just isn't a subclass of what we thought it should be.
+    elsif( !$rslt ) {
+      my $ref = ref $object;
+      $diag = "the extracted object isn't a '$class' it's a '$ref'";
+    }
+  }
+
+  # did we have a 'problem'?
+  my $ok;
+  if($diag)
+  {
+    # print a failure
+    $ok = $Test->ok( 0, $name );
+    # print out the debug info
+    $Test->diag("    $diag\n");
+  }
+  else
+    { $ok = $Test->ok( 1, $name ); }
+
+  # return true unless we printed out a failure
+  return $ok;
+}
+
 =back
 
 And that's that all there is to it, apart from the one function that
@@ -198,5 +368,117 @@ and/or modify it under the same terms as Perl itself.
 L<Test::More>, L<CGI::Untaint>
 
 =cut
+
+
+# code below this point is DIRECTLY cargo culted from Test::More
+# without changing anything
+
+my $DNE = bless [], 'Does::Not::Exist';
+
+sub eq_array  {
+    my($a1, $a2) = @_;
+    return 1 if $a1 eq $a2;
+
+    my $ok = 1;
+    my $max = $#$a1 > $#$a2 ? $#$a1 : $#$a2;
+    for (0..$max) {
+        my $e1 = $_ > $#$a1 ? $DNE : $a1->[$_];
+        my $e2 = $_ > $#$a2 ? $DNE : $a2->[$_];
+
+        push @Data_Stack, { type => 'ARRAY', idx => $_, vals => [$e1, $e2] };
+        $ok = _deep_check($e1,$e2);
+        pop @Data_Stack if $ok;
+
+        last unless $ok;
+    }
+    return $ok;
+}
+
+sub _deep_check {
+    my($e1, $e2) = @_;
+    my $ok = 0;
+
+    my $eq;
+    {
+        # Quiet uninitialized value warnings when comparing undefs.
+        local $^W = 0; 
+
+        if( $e1 eq $e2 ) {
+            $ok = 1;
+        }
+        else {
+            if( UNIVERSAL::isa($e1, 'ARRAY') and
+                UNIVERSAL::isa($e2, 'ARRAY') )
+            {
+                $ok = eq_array($e1, $e2);
+            }
+            elsif( UNIVERSAL::isa($e1, 'HASH') and
+                   UNIVERSAL::isa($e2, 'HASH') )
+            {
+                $ok = eq_hash($e1, $e2);
+            }
+            elsif( UNIVERSAL::isa($e1, 'REF') and
+                   UNIVERSAL::isa($e2, 'REF') )
+            {
+                push @Data_Stack, { type => 'REF', vals => [$e1, $e2] };
+                $ok = _deep_check($$e1, $$e2);
+                pop @Data_Stack if $ok;
+            }
+            elsif( UNIVERSAL::isa($e1, 'SCALAR') and
+                   UNIVERSAL::isa($e2, 'SCALAR') )
+            {
+                push @Data_Stack, { type => 'REF', vals => [$e1, $e2] };
+                $ok = _deep_check($$e1, $$e2);
+            }
+            else {
+                push @Data_Stack, { vals => [$e1, $e2] };
+                $ok = 0;
+            }
+        }
+    }
+
+    return $ok;
+}
+
+sub _format_stack {
+    my(@Stack) = @_;
+
+    my $var = '$FOO';
+    my $did_arrow = 0;
+    foreach my $entry (@Stack) {
+        my $type = $entry->{type} || '';
+        my $idx  = $entry->{'idx'};
+        if( $type eq 'HASH' ) {
+            $var .= "->" unless $did_arrow++;
+            $var .= "{$idx}";
+        }
+        elsif( $type eq 'ARRAY' ) {
+            $var .= "->" unless $did_arrow++;
+            $var .= "[$idx]";
+        }
+        elsif( $type eq 'REF' ) {
+            $var = "\${$var}";
+        }
+    }
+
+    my @vals = @{$Stack[-1]{vals}}[0,1];
+    my @vars = ();
+    ($vars[0] = $var) =~ s/\$FOO/     \$got/;
+    ($vars[1] = $var) =~ s/\$FOO/\$expected/;
+
+    my $out = "Structures begin differing at:\n";
+    foreach my $idx (0..$#vals) {
+        my $val = $vals[$idx];
+        $vals[$idx] = !defined $val ? 'undef' : 
+                      $val eq $DNE  ? "Does not exist"
+                                    : "'$val'";
+    }
+
+    $out .= "$vars[0] = $vals[0]\n";
+    $out .= "$vars[1] = $vals[1]\n";
+
+    $out =~ s/^/    /msg;
+    return $out;
+}
 
 1;
